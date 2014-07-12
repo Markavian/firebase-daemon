20ad477c0a9df6ec71480adde1255673e04939bf
John Beech <github@mkv25.net>
2014-07-12 20:01:42 +0100

    Added specs to try and define the read and write aspects of the daemon based on supplied config.

--- spec/firebase_daemon_spec.js
+++ spec/firebase_daemon_spec.js
@@ -23,6 +23,48 @@ describe("Firebase Daemon", function() {
 			expect(daemon.report()).toEqual({updates: 0});
 		});
 	});
+	
+	describe("addConfig() and listConfigs()", function() {
+		
+		var result = null;
+		var validConfig = {
+			'path': 'path/to/listen-on/',
+			'matcher': 'some/data/structure/values/*',
+			'targets': [
+				'first/path/to/write/to/',
+				'second/path/to/write/to/'
+			]
+		};
+		var invalidPathConfig = {
+		};
+		var invalidMatcherConfig = {
+			'path': 'some path'
+		};
+		var invalidTargetsConfig = {
+			'path': 'some path',
+			'matcher': 'some matcher'
+		};
+		
+		beforeEach(function() {
+			result = daemon.listConfigs();
+		});
+		
+		it("should return an empty array by default", function() {
+			expect(result).toEqual([]);
+		});
+		
+		it("should return a configuration if a valid configuration has been added", function() {
+			daemon.addConfig(validConfig);
+			expect(result).toEqual([validConfig]);
+		});
+		
+		it("should throw errors for invalid configs", function() {
+			expect(function() { daemon.addConfig(invalidPathConfig) }).toThrow(new Error("Invalid config: no path set"));
+			expect(function() { daemon.addConfig(invalidMatcherConfig) }).toThrow(new Error("Invalid config: no matcher set"));
+			expect(function() { daemon.addConfig(invalidTargetsConfig) }).toThrow(new Error("Invalid config: no targets set"));
+			expect(result).toEqual([]);
+		});
+	});
 
 	describe("listen()", function() {
 		var mockChild = { "on": jasmine.createSpy('child.on') };
@@ -37,6 +79,70 @@ describe("Firebase Daemon", function() {
 		it("should register to listen on expected firebase node for changes", function() {
 			expect(firebase.child).toHaveBeenCalledWith(expectedPath);
 			expect(mockChild.on).toHaveBeenCalledWith('value', daemon.handleResponse);
+		});
+	});
+
+	describe("handleResponse()", function() {
+		var snapshotStructure = {
+			"some": {
+				"data": {
+					"structure": {
+						"values": [
+							"beacon1",
+							"beacon2",
+							"beacon3"
+						]
+					}
+				}
+			}
+		};
+		var mockSnapshot = {"val": jasmine.createSpy('snapshot.val').and.returnValue(snapshotStructure)};
+		var mockTargetChildren = {"target/data/structure/beacon1": true, "target/data/structure/beacon2": true};
+		var mockTarget = {
+			"hasChild": function(path) { 
+				return mockTargetChildren[path];
+			}
+		};
+		
+		beforeEach(function() {
+			spyOn(daemon, 'writeResponse');
+			spyOn(daemon, 'listConfigs').and.callThrough();
+			spyOn(mockTarget, 'hasChild').and.callThrough();
+			daemon.handleResponse(mockSnapshot);
+		});
+	
+		it("should test values in path based against configurations", function() {
+			expect(mockSnapshot.val).toHaveBeenCalled();
+			expect(daemon.listConfigs).toHaveBeenCalled();
+			
+			expect(mockTarget.hasChild).toHaveBeenCalledWith("target/data/structure/beacon1");
+			expect(mockTarget.hasChild).toHaveBeenCalledWith("target/data/structure/beacon2");
+			expect(mockTarget.hasChild).toHaveBeenCalledWith("target/data/structure/beacon3");
+			pending();
+		});
+	
+		it("should write missing values to configuration target", function() {
+			expect(daemon.writeResponse).toHaveBeenCalledWith('path/to/write/to/', 'beacon3');
+			pending();
+		});
+	});
+	
+	describe("writeResponse()", function() {
+		beforeEach(function() {
+			daemon.writeResponse("x", "y");
+		});
+		
+		it("should increment the updates counter for each written response", function() {
+			expect(daemon.updates).toEqual(1);
+			
+			for(var i=0; i<10; i++) {
+				daemon.writeResponse("x", "y");
+			}
+			expect(daemon.updates).toEqual(11);
+		});
+		
+		it("should write data to the firebase instance", function() {
+			pending();
 		});
 	});
   
--- src/main/daemon.js
+++ src/main/daemon.js
@@ -3,7 +3,15 @@ var FirebaseDaemon = (function() {
 	var Class = function (firebase) {
 		this.firebase = firebase;
 		this.updates = 0;
+		
+		this._configs = [];
 	};
+	
+	Class.prototype.errors = {
+		"A" : "Invalid config: no path set",
+		"B" : "Invalid config: no matcher set",
+		"C" : "Invalid config: no targets set"
+	}
 	
 	Class.prototype.report = function() {
 		return {
@@ -11,14 +19,35 @@ var FirebaseDaemon = (function() {
 		};
 	};
 	
+	Class.prototype.addConfig = function(config) {
+		if(!config.path) throw new Error(this.errors.A);
+		if(!config.matcher) throw new Error(this.errors.B);
+		if(!config.targets && !(config.targets instanceof Array)) throw new Error(this.errors.C);
+		
+		this._configs.push(config);
+	}
+	
+	Class.prototype.listConfigs = function() {
+		return this._configs;
+	}
+	
 	Class.prototype.listen = function(path) {
 		this.firebase.child(path).on('value', this.handleResponse);
 	};
 	
 	Class.prototype.handleResponse = function(snapshot) {
 		// TODO: Monitor specific value for changes
+		var values = snapshot.val();
+		
+		var configs = this.listConfigs();
+		
 		// TODO: Write monitored value to new locations
+		this.writeResponse('x', 'y');
 	};
+	
+	Class.prototype.writeResponse = function(path, value) {
+		this.updates++;
+	}
 	
 	return Class;
 	
